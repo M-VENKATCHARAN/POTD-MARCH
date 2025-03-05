90fb04a5af527c7cc2b8323511180b41ffa31277
MADDINALA VENKAT CHARAN <151936434+M-VENKATCHARAN@users.noreply.github.com>
2025-03-05 18:57:05 +0530

    POTD MARCH
    2579

---
+++ 5.cpp
@@ -0,0 +1,13 @@
+class Solution {
+    public:
+        long long coloredCells(int n) {
+            long long ans=1;
+            long long val=4;
+            if(n==1)return 1;
+            for(int i=2;i<=n;i++){
+                ans+=val;
+                val=i*4;
+            }
+            return ans;
+        }
+    };
